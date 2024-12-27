2768ba9c99e98c17df34da0d3c6d5cf4c1c93985
akashreddy-ops <danapanaakashreddy999@gmail.com>
2024-12-27 14:39:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+{/* <script> */}
+        const display = document.getElementById('display');
+
+        function appendValue(value) {
+            display.value += value;
+        }
+
+        function clearDisplay() {
+            display.value = '';
+        }
+
+        function calculateResult() {
+            try {
+                display.value = eval(display.value);
+            } catch (error) {
+                display.value = 'Error';
+            }
+        }
+    {/* </script> */}
